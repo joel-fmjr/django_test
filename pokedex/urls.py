05992aeec12085551a6b3503c1d7ca1c3815efd0
Joel J <joel.j@escolar.ifrn.edu.br>
2022-07-19 15:55:33 -0300

    fix login redirect bug

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -3,11 +3,11 @@ from .views import *
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('login/', CustomLoginView.as_view(), name='login'),
+    path('', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', RegisterPage.as_view(), name='register'),
 
-    path('', PokemonList.as_view(), name='pokedex'),
+    path('pokedex', PokemonList.as_view(), name='pokedex'),
     path('pokemon-register/', PokemonCreate.as_view(), name='pokemon-register'),
     path('pokedex-register/', PokedexCreate.as_view(), name='pokedex-register'),
     path('pokemon-update/<int:pk>/', PokemonUpdate.as_view(), name='pokemon-update'),
--- pokedex/views.py
+++ pokedex/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
-
 from django.urls import reverse_lazy
-
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
